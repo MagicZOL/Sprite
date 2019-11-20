cc553d25abf7dc7806757dec85db6a0426589820
MagicZL0 <tnwls9721@naver.com>
2019-11-20 18:00:30 +0900

    첫번째

---
+++ Assets/Scripts/ScrollObject.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollObject : MonoBehaviour
+{
+    [SerializeField] float speed = 1.0f;     //속도
+    [SerializeField] float startPosition;   //나갔다가 다시 나타날위치
+    [SerializeField] float endPosiotion;    //스크롤되서 나간지점
+
+    void Update()
+    {
+        transform.Translate(-1 * speed * Time.deltaTime, 0, 0); //왼쪽 방향으로 이동
+
+        if(transform.position.x <= endPosiotion) //나간지점에 도달했다면
+        {
+            transform.Translate(-1 * (endPosiotion - startPosition), 0, 0);
+        }
+    }
+}
